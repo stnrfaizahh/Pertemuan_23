cebf204889c6c958922e8105d602b2c918e0c5d1
stnrfaizahh <nurfaizahh234@gmail.com>
2024-08-15 00:23:48 +0700

    Level_nama update

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -9,8 +9,10 @@ class LevelController extends Controller
 {
     public function index()
     {
-        DB::insert('insert into m_level(level_kode, level_nama, created_at) values(?, ?, ?)', ['cus', 'pelanggan', now()]);
+        //DB::insert('insert into m_level(level_kode, level_nama, created_at) values(?, ?, ?)', ['cus', 'pelanggan', now()]);
+        //return 'Insert data baru berhasil'
 
-        return 'Insert data baru berhasil';
+        $row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['customer', 'CUS']);
+        return 'Update data berhasil. Jumlah data yang diupdate: ' . $row. ' baris';
     }
 }
