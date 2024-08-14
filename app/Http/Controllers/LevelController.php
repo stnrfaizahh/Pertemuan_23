acf5fa4ab533ed912e1ef959f930925b0a591a9d
stnrfaizahh <nurfaizahh234@gmail.com>
2024-08-15 00:42:27 +0700

    data level pengguna

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -9,13 +9,19 @@ class LevelController extends Controller
 {
     public function index()
     {
+        // //Tambah data
         //DB::insert('insert into m_level(level_kode, level_nama, created_at) values(?, ?, ?)', ['cus', 'pelanggan', now()]);
         //return 'Insert data baru berhasil';
 
+        // //Update data
         //$row = DB::update('updatem_level set level_nama = ? where level_kode = ?', ['customer', 'CUS']);
         //return 'Update data berhasil. Jumlah data yang diupdate: ' . $row. 'baris';
 
-        $row = DB::update('delete from m_level where level_kode = ?', ['CUS']);
-        return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row. ' baris';
+        // //Delete data
+        //$row = DB::delete('delete from m_level where level_kode=?',['cus']);
+        //return 'delete data berhasil, jumlah data yang dihapus' .$row. 'baris';
+
+       $data = DB::select('select * from m_level');
+        return view( 'level',['data'=>$data]);
     }
 }
