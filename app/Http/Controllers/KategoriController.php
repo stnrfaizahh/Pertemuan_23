01e6e156497fc78518890eef5330915480e3880d
stnrfaizahh <nurfaizahh234@gmail.com>
2024-08-15 00:55:29 +0700

    update m_kategori

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -9,13 +9,18 @@ class KategoriController extends Controller
 {
     //
     public function index(){
-        $data=[
-            'kategori_kode'=>'SNK',
-            'kategori_nama'=>'Snack/makanan ringan',
-            'created_at'=>now()
-        ];
-        DB::table('m_kategori')->insert($data);
-        //DB::table('m_kategori')->insert($data);
-        return 'data berhasil ditambah';
+        //Menambah data
+        // $data=[
+        //     'kategori_kode'=>'SNK',
+        //     'kategori_nama'=>'Snack/makanan ringan',
+        //     'created_at'=>now()
+        // ];
+        // DB::table('m_kategori')->insert($data);
+        // //DB::table('m_kategori')->insert($data);
+        // return 'data berhasil ditambah';
+
+        //update data
+        $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Camilan']);
+        return 'update data berhasil, jumlah data yang terupdate' . $row. ' baris';
     }
 }
