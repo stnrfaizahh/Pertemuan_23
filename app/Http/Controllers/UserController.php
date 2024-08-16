cfe7932c803e9b7cf54f4d1991dd64c99ab1263c
stnrfaizahh <nurfaizahh234@gmail.com>
2024-08-16 13:17:21 +0700

    update user faizah

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,11 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
+        $data=
+        [
+            'username'=>'Faizah',
+            'nama'=>'faizah',
+            'password'=>Hash::make('12345'),
+            'user_id'=>4,
+            'level_id'=>1
+        ];
+        UserModel::insert($data);
+        
         //coba akses model UserModel
         $user = UserModel::all(); //ambil semua data dari tabel m_user
         return view('user', ['data'=> $user]);
